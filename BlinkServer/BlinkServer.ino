2fd2f9ad01f6ddc1934ff52774c63676d732546d
Lasse Lukkari <lasse.lukkari@gmail.com>
2019-01-18 16:49:44 +0200

    API for reading and updating the LED state

--- BlinkServer/BlinkServer.ino
+++ BlinkServer/BlinkServer.ino
@@ -3,13 +3,28 @@
 
 #define WIFI_SSID "network"
 #define WIFI_PASSWORD "password"
+#define LED_BUILTIN 2
 
 WiFiServer server(80);
 WebApp app;
+bool ledOn;
+
+void readLed(Request &req, Response &res) {
+  res.success("text/plain");
+  res.print(ledOn);
+}
+
+void updateLed(Request &req, Response &res) {
+  ledOn = (req.read() != '0');
+  digitalWrite(LED_BUILTIN, ledOn);
+  return readLed(req, res);
+}
 
 void setup() {
   Serial.begin(115200);
-  
+
+  pinMode(LED_BUILTIN, OUTPUT);
+
   WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
   while (WiFi.status() != WL_CONNECTED) {
     delay(500);
@@ -17,8 +32,10 @@ void setup() {
   }
   Serial.println(WiFi.localIP());
 
+  app.get("/led", &readLed);
+  app.put("/led", &updateLed);
   app.use(staticFiles());
-  
+
   server.begin();
 }
 
